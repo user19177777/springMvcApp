7ce566bc5c79a2fd6d3fab0e1249ed58e6fe9698
Ivan Ivanov <79523701529@yandex.ru>
2022-08-01 14:50:56 +0300

    @RequestParam когда нужны только параметры, а не ответ полностью

--- src/main/java/springMvcApp/HelloController.java
+++ src/main/java/springMvcApp/HelloController.java
@@ -2,14 +2,15 @@ package springMvcApp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String sayHello(HttpServletRequest request){
-        String name = request.getParameter("name");
+    public String sayHello(@RequestParam("name")String name){
+
         System.out.println(name);
         return "hello";
     }
