6ecd96645f2b513706aaf886a59c0e85f7b824d5
Ivan Ivanov <79523701529@yandex.ru>
2022-08-01 14:48:41 +0300

    HttpServletRequest когда нужен ответ полностью

--- src/main/java/springMvcApp/HelloController.java
+++ src/main/java/springMvcApp/HelloController.java
@@ -3,10 +3,14 @@ package springMvcApp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello(HttpServletRequest request){
+        String name = request.getParameter("name");
+        System.out.println(name);
         return "hello";
     }
 }
